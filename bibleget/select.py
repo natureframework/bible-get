99af209152c75daf3b10d56fe75c8d74007c16e2
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-06-28 23:37:36 +0800

    Fix select

--- bibleget/select.py
+++ bibleget/select.py
@@ -3,8 +3,17 @@ from pandas import DataFrame
 
 
 def select(book: DataFrame, reference: Reference) -> DataFrame:
-    chapters = book["chapter"]
-    verses = book["verse"]
-    start = (chapters >= reference.start.chapter) & (verses >= reference.start.verse)
-    end = (chapters <= reference.end.chapter) & (verses <= reference.end.verse)
-    return book[start & end]
+    chapters = book["chapter"].astype(str)
+    verses = book["verse"].astype(str)
+    width = max(chapters.str.len().max(), verses.str.len().max())
+    chapters = chapters.str.zfill(width)
+    verses = verses.str.zfill(width)
+    index = chapters.str.cat(verses).astype(int)
+    start = int(
+        str(reference.start.chapter).zfill(width)
+        + str(reference.start.verse).zfill(width)
+    )
+    end = int(
+        str(reference.end.chapter).zfill(width) + str(reference.end.verse).zfill(width)
+    )
+    return book[index.between(start, end)]
